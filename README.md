4d0218c35d91851e9d4d81e8ecfbe18d41e2fc49
strood <stroodz19@gmail.com>
2020-06-09 18:51:49 -0600

    Phase 1: Basic server complete

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
-# RailsLite 
+# RailsLite
 
 -Lite version of Ruby on Rails built as an  AppAcademy project.
 
+Learning Goals
 
+- Know how the basics of the HTTP request-response cycle works
+- Know how a server works at a basic level
+- Be able to explain the functionality of the ControllerBase class
+- Know how cookies are used by browsers and servers
+- Be able to write a router that parses a URL and instantiates the appropriate controller
+- Know how params get built out of the HTTP request's information
--- bin/p01_basic_server.rb
+++ bin/p01_basic_server.rb
@@ -1,1 +1,39 @@
 require 'rack'
+# ack is a middleware that sits between a web server and a web application
+# framework to make writing frameworks and web servers that work with existing
+# software easier. We can make a functional server with only a few lines of code
+# from the Rack module.
+
+# Example of super basic app, forming our own response instead of using Rack
+
+# app = Proc.new do |env|
+#   ['200', {'Content-Type' => 'text/html'}, ["Hello World"]]
+# end
+
+# App built with Rack (Same as one above but uses rack)
+
+# app = Proc.new do |env|
+#   req = Rack::Request.new(env)
+#   res = Rack::Response.new
+#   res['Content-Type'] = 'text/html'
+#   res.write('Hello World Again')
+#   res.finish
+# end
+
+# Actual app we will use in server, we want to respond to requests with a
+# string of the requested path
+#  ie. localhost:3000/i/love/app/academy I want it to display "/i/love/app/academy"
+app = Proc.new do |env|
+  req = Rack::Request.new(env)
+  res = Rack::Response.new
+  res['Content-Type'] = 'text/html'
+  request_path = req.path
+
+  res.write(request_path)
+  res.finish
+end
+
+Rack::Server.start({
+  app: app,
+  Port: 3000
+  })
